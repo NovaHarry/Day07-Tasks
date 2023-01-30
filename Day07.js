ba719e626c152658449dd1bf47a7d23badda4c45
NovaHarry <novaharry7@gmail.com>
2023-01-30 06:53:50 -0800

    Second Commit

--- Day07.js
+++ Day07.js
@@ -5,34 +5,34 @@ request.onload=function()
 {
     var result=JSON.parse(request.response);
 
-// //A. Get all the countries from the Asia continent /region using the Filter function:
+//A. Get all the countries from the Asia continent /region using the Filter function:
 
-// let a = result.filter((arr)=> {return arr.region=='Asia'});
+let a = result.filter((arr)=> {return arr.region=='Asia'});
 
-// console.log(a);
+console.log(a);
  
-// //B. Get all the countries with a population of less than 2 lakhs using Filter function:
+//B. Get all the countries with a population of less than 2 lakhs using Filter function:
 
-// let b = result.filter((arr)=> {return arr.population < 200000});
+let b = result.filter((arr)=> {return arr.population < 200000});
 
-// console.log(b);
+console.log(b);
 
-// // C. Print the following details name, capital, flag using forEach function :
+// C. Print the following details name, capital, flag using forEach function :
 
-// let c = result.forEach((arr)=> 
-// {
-//     console.log(arr.name);
-//     console.log("Capital:" + arr.capital);
-//     console.log("Flag:" + arr.flag);
-// });
+let c = result.forEach((arr)=> 
+{
+    console.log(arr.name);
+    console.log("Capital:" + arr.capital);
+    console.log("Flag:" + arr.flag);
+});
 
-// //D. Print the total population of countries using reduce function:
+//D. Print the total population of countries using reduce function:
 
-// let d = result.reduce((ini,curr) => ini+curr.population,0);
+let d = result.reduce((ini,curr) => ini+curr.population,0);
 
-// console.log("Total Population:" + d);
+console.log("Total Population:" + d);
 
-//E. Print the country which uses US Dollars as currency :
+// E. Print the country which uses US Dollars as currency :
 
 let e = result;
 let curr = [];
@@ -55,8 +55,3 @@ for(var i in curr)
 }
 
 }
-
-
-
-
-
